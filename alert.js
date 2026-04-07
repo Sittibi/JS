37699e565ba01fe12ecfa0907de6d648b9cc1d91
Stanislav <sitibione@gmail.com>
2026-04-07 14:51:54 +0300

    ZadachaComplite

--- alert.js
+++ alert.js
@@ -1,12 +1,16 @@
-const stavka = 80;
+const stavkaUSD = 80;
 const hoursRdy = 5;
 const days = 5;
 const hoursWork = 40;
 const needToGO = 11;
+const availeblesDays = 11 - 2;
 let hoursHave;
-hoursHave = 5 * 10;
-hoursHave > hoursWork;
-console.log(hoursHave)
+hoursHave = 5 * availeblesDays;
+let oplata;
+console.log('Смогу ли я работь?' + ' ' + (hoursHave > hoursWork));
+oplata = hoursWork * stavkaUSD
+console.log(oplata + ' ' +'USD');
+
 
 
 
