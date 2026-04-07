4fe948439a3c718b976a593ce6b848c5d49785ae
Stanislav <sitibione@gmail.com>
2026-04-07 14:36:47 +0300

    Zadacha

--- alert.js
+++ alert.js
@@ -3,7 +3,10 @@ const hoursRdy = 5;
 const days = 5;
 const hoursWork = 40;
 const needToGO = 11;
-let timeWaste;
-let daysHave;
+let hoursHave;
+hoursHave = 5 * 10;
+hoursHave > hoursWork;
+console.log(hoursHave)
+
 
 
