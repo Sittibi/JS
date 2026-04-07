1ae9c51c443fbab867203fda70ad2ed3c817bc86
Stanislav <sitibione@gmail.com>
2026-04-07 16:44:15 +0300

    Zadacha v2.1

--- alert.js
+++ alert.js
@@ -2,10 +2,11 @@ const money = 12000;
 const yeahrProcent = 0.07;
 const moneyPerMonth = (money * yeahrProcent) / 12;
 const fullPayment = money + moneyPerMonth * 24;
+const houseCost = 13500;
 
-if 
-
-
-
-
+if (fullPayment > houseCost) {
+    console.log(`Мы накопили: ${fullPayment}$. Ура, я куплю дом. Остаток ${fullPayment - houseCost}$.`);
+} else {
+  console.log(`Столько копил, а денег не хватило на дом. Ушла в минус на ${fullPayment - houseCost}$ =(`);
+}
 
