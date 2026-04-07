ebc53df70d0697b8882e5cae2aa47498b0e186ac
Stanislav <sitibione@gmail.com>
2026-04-07 15:41:58 +0300

    Boolean false все значения

--- alert.js
+++ alert.js
@@ -1,14 +1,8 @@
-const age = '18';
-// со знаком + не преобразут строку в число. Number селектор преобразования.
-console.log(Number(age) + 5);
-// c другими значниями - * / автоматически переводит строку в число
-console.log(age - 3);
-// String селектор преобразования в строку.
-console.log(String(4) + 3);
-
-// Boolean имеет числовое значение false = 0 true = 1
-console.log(Boolean('') + 3);
-console.log(true + 3);
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(Number('sca'))); // NaN not an number
 
 
 
