cb9c5bd97f0738ebf4e6b20033700722b1b156be
Stanislav <sitibione@gmail.com>
2026-04-08 13:07:53 +0300

    3.3

--- alert.js
+++ alert.js
@@ -1,11 +1,11 @@
 const plus = 22;
 const minus = -8;
 const robot = 'Я не робот';
-const answer = prompt('Введите число');
+let answer = prompt('Введите число');
 
-switch (answer) {
-case plus:
-case minus:
-case robot:
+switch (true) {
+case answer = plus:22
+case answer = minus:
+case answer = robot:
   console.log('Успех')
 }
