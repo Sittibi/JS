869764a38a377abcc6660d0eb980a1dfcebc9991
Stanislav <sitibione@gmail.com>
2026-04-08 13:31:30 +0300

    Zada4a 3 compliete

--- alert.js
+++ alert.js
@@ -1,11 +1,11 @@
-const plus = 22;
-const minus = -8;
-const robot = 'Я не робот';
-let answer = prompt('Введите число');
+let res = prompt("Сколько будет 7 + или - 15");
 
 switch (true) {
-case answer = plus:22
-case answer = minus:
-case answer = robot:
-  console.log('Успех')
+  case res === "я не роборт":
+  case Number(res) === 22:
+  case Number(res) === -8:
+    console.log("Успех");
+    break;22
+  default:
+    console.log("Вы робот");
 }
